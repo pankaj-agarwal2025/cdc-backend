8bf5a167cb46ea2584669636d4fa512b5147b0e5
khushnam-chauhan <chauhankhushnam@gmail.com>
2025-04-22 14:54:11 +0530

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "cdc.soet-krmu.com",
+      "https://cdc.soet-krmu.com",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
